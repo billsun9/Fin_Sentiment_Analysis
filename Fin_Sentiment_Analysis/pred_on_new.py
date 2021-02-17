9e2f28384aed7632f404f79ae7a29a3f9940de45
bill <billsun9@gmail.com>
2021-02-17 00:29:29 -0500

    added bert

--- Fin_Sentiment_Analysis/pred_on_new.py
+++ Fin_Sentiment_Analysis/pred_on_new.py
@@ -3,18 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # %%
-# load
+# for custom models
 model_single = tf.keras.models.load_model('models/rcnn_single_layer/')
 model_layered = tf.keras.models.load_model('models/rcnn_multi_layered/')
-# %%
-s1 = ('A deep freeze enveloping large swathes of the U.S. has resulted in rolling blackouts for at least 5M people from the upper Midwest to Houston. More than a million barrels a day of oil and 10B cubic feet of gas production have also gone offline, sending U.S. crude prices above $60 a barrel for the first time in more than year and natural gas prices up 6%. This is a terrible event. Pipelines have also declared force majeure, while massive refineries owned by Exxon Mobil (NYSE:XOM) and Marathon Petroleum (NYSE:MPC) have halted production, threatening to reduce supplies of gasoline and diesel across the country.')
-predictions = model_layered.predict(np.array([s1]))
-print(predictions)
-s2 = ('The movie was not good. The animation and the graphics '
-                    'were terrible. I would not recommend this movie.')
-predictions = model_layered.predict(np.array([s2]))
-print(predictions)
-# %%
-def pred_on_new(s): # Str input; Returns prediction
+
+
+def pred_on_new_custom(s): # Str input; Returns prediction
     input = (s)
     return model_layered.predict(np.array([input]))
+
+# %%
+# for finetuned bert model 
+
+bert_model = tf.saved_model.load('models/finetuned_bert/')
+
+def pred_on_new_bert(s): # Str input; Returns prediction
+    input = [s]
+    return tf.sigmoid(bert_model(tf.constant(input)))
---
+++ Fin_Sentiment_Analysis/train_finetune_bert.py
@@ -0,0 +1,154 @@
+# pip install -q tensorflow-text
+# pip install -q tf-models-official
+
+import os
+import shutil
+
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_text as text
+from official.nlp import optimization  # to create AdamW optmizer
+
+import matplotlib.pyplot as plt
+# %%
+# load data
+url = 'https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz'
+
+dataset = tf.keras.utils.get_file('aclImdb_v1.tar.gz', url,
+                                  untar=True, cache_dir='.',
+                                  cache_subdir='')
+
+dataset_dir = os.path.join(os.path.dirname(dataset), 'aclImdb')
+
+train_dir = os.path.join(dataset_dir, 'train')
+
+# remove unused folders to make it easier to load the data
+remove_dir = os.path.join(train_dir, 'unsup')
+shutil.rmtree(remove_dir)
+# %%
+# create validation dataset and shuffle
+AUTOTUNE = tf.data.AUTOTUNE
+batch_size = 32
+seed = 42
+
+raw_train_ds = tf.keras.preprocessing.text_dataset_from_directory(
+    'aclImdb/train',
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='training',
+    seed=seed)
+
+class_names = raw_train_ds.class_names
+train_ds = raw_train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+val_ds = tf.keras.preprocessing.text_dataset_from_directory(
+    'aclImdb/train',
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='validation',
+    seed=seed)
+
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+test_ds = tf.keras.preprocessing.text_dataset_from_directory(
+    'aclImdb/test',
+    batch_size=batch_size)
+
+test_ds = test_ds.cache().prefetch(buffer_size=AUTOTUNE)
+# %%
+# view a few example texts & labels
+for text_batch, label_batch in train_ds.take(1):
+  for i in range(3):
+    print(f'Review: {text_batch.numpy()[i]}')
+    label = label_batch.numpy()[i]
+    print(f'Label : {label} ({class_names[label]})')
+# %%
+# choose bert model
+
+from mappings import map_model_to_preprocess, map_name_to_handle
+
+bert_model_name = 'small_bert/bert_en_uncased_L-4_H-512_A-8'
+
+tfhub_handle_encoder = map_name_to_handle[bert_model_name]
+tfhub_handle_preprocess = map_model_to_preprocess[bert_model_name]
+
+print(f'BERT model selected           : {tfhub_handle_encoder}')
+print(f'Preprocess model auto-selected: {tfhub_handle_preprocess}')
+
+bert_preprocess_model = hub.KerasLayer(tfhub_handle_preprocess)
+# %%
+def build_classifier_model():
+  text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
+  preprocessing_layer = hub.KerasLayer(tfhub_handle_preprocess, name='preprocessing')
+  encoder_inputs = preprocessing_layer(text_input)
+  encoder = hub.KerasLayer(tfhub_handle_encoder, trainable=True, name='BERT_encoder')
+  outputs = encoder(encoder_inputs)
+  net = outputs['pooled_output']
+  net = tf.keras.layers.Dropout(0.1)(net)
+  net = tf.keras.layers.Dense(1, activation=None, name='classifier')(net)
+  return tf.keras.Model(text_input, net)
+
+# %%
+classifier_model = build_classifier_model()
+loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+metrics = tf.metrics.BinaryAccuracy()
+epochs = 5
+steps_per_epoch = tf.data.experimental.cardinality(train_ds).numpy()
+num_train_steps = steps_per_epoch * epochs
+num_warmup_steps = int(0.1*num_train_steps)
+
+init_lr = 3e-5
+optimizer = optimization.create_optimizer(init_lr=init_lr,
+                                          num_train_steps=num_train_steps,
+                                          num_warmup_steps=num_warmup_steps,
+                                          optimizer_type='adamw')
+
+classifier_model.compile(optimizer=optimizer,
+                         loss=loss,
+                         metrics=metrics)
+
+# TRAIN
+history = classifier_model.fit(x=train_ds,
+                               validation_data=val_ds,
+                               epochs=epochs)
+
+# EVALUATE
+loss, accuracy = classifier_model.evaluate(test_ds)
+
+print(f'Loss: {loss}')
+print(f'Accuracy: {accuracy}')
+# graphs of loss/acc vs epoch
+history_dict = history.history
+print(history_dict.keys())
+
+acc = history_dict['binary_accuracy']
+val_acc = history_dict['val_binary_accuracy']
+loss = history_dict['loss']
+val_loss = history_dict['val_loss']
+
+epochs = range(1, len(acc) + 1)
+fig = plt.figure(figsize=(10, 6))
+fig.tight_layout()
+
+plt.subplot(2, 1, 1)
+# "bo" is for "blue dot"
+plt.plot(epochs, loss, 'r', label='Training loss')
+# b is for "solid blue line"
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+# plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.subplot(2, 1, 2)
+plt.plot(epochs, acc, 'r', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+# %%
+example = ['I have never seen a movie so unpleasant in my entire life. I truly found nothing good about the sound effects and the choreography']
+
+results = tf.sigmoid(classifier_model(tf.constant(example)))
+print(results) # returns 0.017854
